c9a0efd3689b92346cadb286e0416448f31cbf89
BaasS <baass318@gmail.com>
2018-09-27 17:39:14 +0200

    Updated styling. Added images.

--- functions.js
+++ functions.js
@@ -2,8 +2,8 @@ function enterCode1() {
     var input = document.getElementById("codeInput1").value;
     var code = "1234";
     if (input == code) {
-        document.getElementById("feedback1").innerHTML = "YES";
+        document.getElementById("feedback1").innerHTML = "Ja, das goed!";
     } else {
-        document.getElementById("feedback1").innerHTML = "NO";
+        document.getElementById("feedback1").innerHTML = "Nee. Fout.";
     }
 }
